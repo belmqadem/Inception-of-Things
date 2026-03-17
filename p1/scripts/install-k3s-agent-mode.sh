8bce62c0718232052477ac9b6f31c64b50560b47
belmqadem <adilfront3@gmail.com>
2026-03-17 04:50:24 +0000

    add: scripts to install K3s agent and controller modes

---
+++ p1/scripts/install-k3s-agent-mode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOKEN=$(cat /vagrant/node-token)
+
+curl -sfL https://get.k3s.io | \
+  K3S_TOKEN="$TOKEN" \
+  INSTALL_K3S_EXEC="agent \
+    --server=https://192.168.56.110:6443 \
+    --node-ip=192.168.56.111 \
+    --flannel-iface=eth1" \
+  sh -
+
+echo "K3s agent is ready!"
