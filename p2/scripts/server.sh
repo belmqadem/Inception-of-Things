35013119ca88e2e25e9fada7a42c5de4c563ad43
belmqadem <adilfront3@gmail.com>
2026-03-18 05:14:13 +0000

    fix: update nginx image to version 1.27-alpine for app deployments; enhance server script with error handling

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 curl -sfL https://get.k3s.io | \
   INSTALL_K3S_EXEC="server \
     --bind-address=192.168.56.110 \
@@ -7,5 +9,9 @@ curl -sfL https://get.k3s.io | \
     --flannel-iface=eth1 \
     --write-kubeconfig-mode=644" \
   sh -
+
+sleep 10
+
+kubectl apply -f /vagrant/confs/
 
 echo "K3s server is ready!"
